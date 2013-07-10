432d52334ed791385e25c2e0d6d23d9818389da3
Michael Grassotti <mike@weshop.com>
2013-07-10 03:10:00 -0400

    move ember-test-container style to qunit test harness so that karma debug shows ember app in fullscreen mode

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,5 +1,4 @@
 document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-document.write('<style>#ember-testing-container { position: absolute; background: white; bottom: 0; right: 0; width: 640px; height: 384px; overflow: auto; z-index: 9999; border: 1px solid #ccc; } #ember-testing { zoom: 50%; }</style>');
 
 Ember.testing = true;
 
