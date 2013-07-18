aa687251d8bed702d702249eaa62c0f7ee2468a7
Robert Jackson <robert.w.jackson@me.com>
2013-07-18 15:08:56 -0400

    No need to copy before transpiling.

--- tasks/options/copy.js
+++ tasks/options/copy.js
@@ -1,22 +1,4 @@
 module.exports = {
-  // These copy tasks happen before transpile or hinting. They
-  // prepare the build pipeline by moving JavaScript files to
-  // tmp/javascript.
-  //
-  "prepare": {
-    files: [{
-      expand: true,
-      cwd: 'app/',
-      src: '**/*.js',
-      dest: 'tmp/javascript/app'
-    },
-    {
-      expand: true,
-      cwd: 'tests/',
-      src: ['**/*.js', '!vendor/**/*.js'],
-      dest: 'tmp/javascript/tests/'
-    }]
-  },
   // Stage moves files to their final destinations after the rest
   // of the build cycle has run.
   //
