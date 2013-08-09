0c50ab262801f30034834712c65fb0a437d83a62
Joe Fiorini <joe@joefiorini.com>
2013-08-09 08:00:07 -0400

    Fix QUnit output for equal assertions
    Display a friendlier message than 'undefined' when an equal assertion fails.

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -12,7 +12,13 @@ function exists(selector) {
   return !!find(selector).length;
 }
 
+function equal(actual, expected, message) {
+  message = message || QUnit.jsDump.parse(expected) + " expected but was " + QUnit.jsDump.parse(actual);
+  QUnit.equal.call(this, expected, actual, message);
+}
+
 window.exists = exists;
+window.equal = equal;
 
 Ember.Container.prototype.stub = function(fullName, instance) {
   instance.destroy = instance.destroy || function() {};
