e2610e1eacbffd09fa2d86a05ed9d4139ab402e3
Michael <obrocnikm@gmail.com>
2013-09-08 21:40:41 +0200

    Support for Compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,70 +3,84 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     env: process.env,
   };
-
   grunt.util._.extend(config, loadConfig('./tasks/options/'));
-
   grunt.initConfig(config);
-
   require('load-grunt-tasks')(grunt);
   grunt.loadTasks('tasks');
 
   grunt.registerTask('default', "Build (in debug mode) & test your application.", ['test']);
-  grunt.registerTask('build',   [
-                     'clean:build',
-                     'lock',
-                     // Uncomment this line  & `npm install --save-dev grunt-contrib-coffee` for CoffeeScript support.
-                     // 'coffee',
-                     'copy:prepare',
-                     'transpile',
-                     'jshint',
-                     'copy:stage',
-                     // Uncomment line below & `npm install --save-dev grunt-sass` for SASS (SCSS only) support.
-                     // or run `npm install --save-dev grunt-contrib-sass` for SCSS/SASS support (may be slower).
-                     // 'sass:compile',
-                     // Uncomment this line & `npm install --save-dev grunt-contrib-less` for LESS support.
-                     // 'less:compile'
-                     // Uncomment this line & `npm install --save-dev grunt-contrib-stylus` for stylus/nib support.
-                     // 'stylus:compile'
-                     'concat_sourcemap',
-                     'unlock' ]);
+
+  grunt.registerTask('build', [
+    'clean:build',
+    'lock',
+    // Uncomment line below & `npm install --save-dev grunt-contrib-coffee` 
+    // for CoffeeScript support.
+    // 'coffee',
+    'copy:prepare',
+    'transpile',
+    'jshint',
+    'copy:stage',
+    // Uncomment line below & `npm install --save-dev grunt-sass` for SASS 
+    // (SCSS only) support. Or run `npm install --save-dev grunt-contrib-sass` 
+    // for SCSS/SASS support (may be slower). Delete `public/assets/app.css` 
+    // and create `app/styles/app.scss` instead.
+    // 'sass:compile',
+    // Uncomment line below & `npm install --save-dev grunt-contrib-compass`
+    // for Compass support. You don't need SASS if you have Compass.
+    'compass:compile',
+    // Uncomment line below & `npm install --save-dev grunt-contrib-less` 
+    // for LESS support.
+    // 'less:compile'
+    // Uncomment line below & `npm install --save-dev grunt-contrib-stylus` for stylus/nib support.
+    // 'stylus:compile'
+    'concat_sourcemap',
+    'unlock' ]);
 
   grunt.registerTask('build:debug', "Build a development-friendly version of your app.", [
-                     'build',
-                     'emberTemplates:debug',
-                     'copy:vendor' ]);
+    'build',
+    'emberTemplates:debug',
+    'copy:vendor' ]);
 
   grunt.registerTask('build:dist', "Build a minified & production-ready version of your app.", [
-                     'build',
-                     'clean:release',
-                     'emberTemplates:dist',
-                     'dom_munger:distEmber',
-                     'dom_munger:distHandlebars',
-                     'useminPrepare',
-                     'concat',
-                     'uglify',
-                     'copy:dist',
-                     'rev',
-                     'usemin' ]);
+    'build',
+    'clean:release',
+    'emberTemplates:dist',
+    'dom_munger:distEmber',
+    'dom_munger:distHandlebars',
+    'useminPrepare',
+    'concat',
+    'uglify',
+    'copy:dist',
+    'rev',
+    'usemin' ]);
 
   grunt.registerTask('test', "Run your apps's tests once. Uses Google Chrome by default. Logs coverage output to tmp/public/coverage.", [
-                     'build:debug', 'karma:test' ]);
+    'build:debug', 
+    'karma:test' ]);
 
   grunt.registerTask('test:ci', "Run your app's tests in PhantomJS. For use in continuous integration (i.e. Travis CI).", [
-                     'build:debug', 'karma:ci' ]);
+    'build:debug', 
+    'karma:ci' ]);
 
   grunt.registerTask('test:browsers', "Run your app's tests in multiple browsers (see tasks/options/karma.js for configuration).", [
-                     'build:debug', 'karma:browsers' ]);
+    'build:debug', 
+    'karma:browsers' ]);
 
   grunt.registerTask('test:server', "Start a Karma test server. Automatically reruns your tests when files change and logs the results to the terminal.", [
-                     'build:debug', 'karma:server', 'connect:server', 'watch:test']);
+    'build:debug', 
+    'karma:server', 
+    'connect:server', 
+    'watch:test']);
 
   grunt.registerTask('server', "Run your server in development mode, auto-rebuilding when files change.",
-                     ['build:debug', 'connect:server', 'watch:main']);
-  grunt.registerTask('server:dist', "Build and preview production (minified) assets.",
-                     ['build:dist', 'connect:dist:keepalive']);
-};
+    ['build:debug', 
+    'connect:server', 
+    'watch:main']);
 
+  grunt.registerTask('server:dist', "Build and preview production (minified) assets.",
+    ['build:dist', 
+    'connect:dist:keepalive']);
+};
 
 // TODO: extract this out
 function loadConfig(path) {
