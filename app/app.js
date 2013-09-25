23909434b376d0887be23e5159bd9cc85fd0a0e9
Stefan Penner <stefan.penner@gmail.com>
2013-09-25 15:19:31 -0400

    Ensure our router is a subclass of Ember.Router, to work around some global router pollution that may otherwise occur.

--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
 import Resolver from 'resolver';
-import router from 'appkit/router';
+import Router from 'appkit/router';
 
 var App = Ember.Application.extend({
   LOG_ACTIVE_GENERATION: true,
   LOG_VIEW_LOOKUPS: true,
   modulePrefix: 'appkit', // TODO: loaded via config
   Resolver: Resolver,
-  Router: Ember.Router.extend({
-    router: router
-  })
+  Router: Router
 });
 
 export default App;
--- app/router.js
+++ app/router.js
@@ -1,7 +1,9 @@
-var router = Ember.Router.map(function(){
+var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
+
+Router.map(function(){
   // this.resource('posts', function() {
   //   this.route('new');
   // });
 });
 
-export default router;
+export default Router;
