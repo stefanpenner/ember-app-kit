c0ccab89ddf38dcf6a58ffb2d1d1a2191dda4876
Stefan Penner <stefan.penner@gmail.com>
2013-10-01 07:49:38 -0400

    log all the things

--- app/app.js
+++ app/app.js
@@ -2,8 +2,10 @@ import Resolver from 'resolver';
 import registerComponents from 'appkit/utils/register_components';
 
 var App = Ember.Application.extend({
-  LOG_MODULE_RESOLVER: true,
   LOG_ACTIVE_GENERATION: true,
+  LOG_MODULE_RESOLVER: true,
+  LOG_TRANSITIONS: true,
+  LOG_TRANSITIONS_INTERNAL: true,
   LOG_VIEW_LOOKUPS: true,
   modulePrefix: 'appkit', // TODO: loaded via config
   Resolver: Resolver
