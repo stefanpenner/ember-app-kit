85a456ab18ac98bb71d3bfda153e5f49b9518811
Stefan Penner <stefan.penner@gmail.com>
2013-10-03 17:59:41 -0400

    improve example acceptance test

--- tests/acceptance/component_test.js
+++ tests/acceptance/component_test.js
@@ -13,9 +13,11 @@ test("component output is rendered", function(){
   expect(3);
 
   visit('/component-test').then(function(){
-    ok(exists("h2:contains('Welcome to Ember.js')"));
-
+    var title = find('h2#title');
     var list = find(".pretty-color");
+
+    equal(title.text(), 'Welcome to Ember.js');
+
     equal(list.length, 3);
     equal(list.first().text(), "Pretty Color: purple\n");
   });
