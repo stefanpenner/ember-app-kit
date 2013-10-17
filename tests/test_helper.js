37e84b8a5ff2eb99520dd937b3dae3018f862f35
manoharank <manoit88@gmail.com>
2013-10-17 22:03:20 +0530

    Add default assertion message for qunit `strictEqual`

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -9,10 +9,20 @@ function exists(selector) {
   return !!find(selector).length;
 }
 
+function getAssertionMessage(actual, expected, message) {
+  return message || QUnit.jsDump.parse(expected) + " expected but was " + QUnit.jsDump.parse(actual);
+}
+
 function equal(actual, expected, message) {
-  message = message || QUnit.jsDump.parse(expected) + " expected but was " + QUnit.jsDump.parse(actual);
+  message = getAssertionMessage(actual, expected, message);
   QUnit.equal.call(this, actual, expected, message);
+}
+
+function strictEqual(actual, expected, message) {
+  message = getAssertionMessage(actual, expected, message);
+  QUnit.strictEqual.call(this, actual, expected, message);
 }
 
 window.exists = exists;
 window.equal = equal;
+window.strictEqual = strictEqual;
