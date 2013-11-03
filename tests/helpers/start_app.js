290e9370d32758bd2f9b49e46ac0a2dee42fdfa0
Robert Jackson <robert.w.jackson@me.com>
2013-11-02 21:33:27 -0400

    Advance readiness (no need for reset).

--- tests/helpers/start_app.js
+++ tests/helpers/start_app.js
@@ -21,7 +21,7 @@ function startApp(attrs) {
     location: 'hash'
   });
 
-  App.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
+  Ember.run(App, 'advanceReadiness');
 
   return App;
 }
