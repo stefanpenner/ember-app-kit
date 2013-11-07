cc5709abfb001a8d02e81812ed218aecb3fb43f6
Stefan Penner <stefan.penner@gmail.com>
2013-11-06 21:39:17 -0500

    include a useful RSVP on error configuration by default.
    Should increase visibility to crazy issues

--- app/app.js
+++ app/app.js
@@ -10,4 +10,13 @@ var App = Ember.Application.extend({
   Resolver: Resolver.default
 });
 
+Ember.RSVP.configure('onerror', function(error) {
+  // ensure unhandled promises raise awareness.
+  // may result in false negatives, but visibility is more imporant
+  if (error instanceof Error) {
+    console.assert(false, error);
+    console.error(error.stack);
+  }
+});
+
 export default App;
