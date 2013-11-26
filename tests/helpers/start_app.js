afddc76d12a357d5ff093e72244cb489ecdd1db8
Stefan Penner <stefan.penner@gmail.com>
2013-11-26 17:51:15 -0500

    Revert "Removes unnecessary location: none in start_app since this is done by Ember on"
    This reverts commit 0a72317f9b5ff2a80cca882028a9b774abe7f3f8.

--- tests/helpers/start_app.js
+++ tests/helpers/start_app.js
@@ -17,6 +17,10 @@ function startApp(attrs) {
     App.injectTestHelpers();
   });
 
+  Router.reopen({
+    location: 'none'
+  });
+
   App.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
 
   return App;
