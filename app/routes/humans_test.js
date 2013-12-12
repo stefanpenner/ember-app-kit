583a2377b2947d35dfdbd3a606354434a6a71d42
Taras Mankovski <tarasm@gmail.com>
2013-12-12 13:51:48 -0500

    Updated route to use ic-ajax 0.2

--- app/routes/humans_test.js
+++ app/routes/humans_test.js
@@ -1,7 +1,5 @@
 export default Ember.Route.extend({
   model: function() {
-    return ic.ajax('humans.txt').then(function(result){
-      return result.response;
-    }, Ember.RSVP.rethrow);
+    return ic.ajax('humans.txt');
   }
 });
