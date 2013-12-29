b05999fbb6323a2a43c458a9d9c59748d4bf8bc0
Stefan Penner <stefan.penner@gmail.com>
2013-12-29 00:26:58 -0500

    toddy-up start app helper

--- tests/helpers/start_app.js
+++ tests/helpers/start_app.js
@@ -11,14 +11,14 @@ function startApp(attrs) {
     LOG_VIEW_LOOKUPS: false
   }, attrs); // but you can override;
 
-  Ember.run.join(function(){
+  Router.reopen({
+    location: 'none'
+  });
+
+  Ember.run(function(){
     App = Application.create(attributes);
     App.setupForTesting();
     App.injectTestHelpers();
-  });
-
-  Router.reopen({
-    location: 'none'
   });
 
   App.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
