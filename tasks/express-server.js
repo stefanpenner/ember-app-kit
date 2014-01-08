784fdd938ef9f6458093f6772240d1456aa6216e
FilmKnurd <andrew@astonishdesign.com>
2014-01-08 12:02:41 -0600

    Removes space between lines

--- tasks/express-server.js
+++ tasks/express-server.js
@@ -50,7 +50,6 @@ module.exports = function(grunt) {
       app.use(static({ urlRoot: '/vendor', directory: 'vendor' }));
       app.use(static({ directory: 'public' }));
       app.use(static({ urlRoot: '/tests', directory: 'tests' })); // For test_helper.js and test_loader.js
-
       app.use(static({ directory: 'tmp/result' }));
       app.use(static({ file: 'tmp/result/index.html' })); // Gotta catch 'em all
     } else {
