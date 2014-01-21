f662e8b26f46c72ebbe6f20ba02ee09ecc6854aa
Ulises Ramirez-Roche <ulisesrmzroche@gmail.com>
2014-01-21 13:49:17 -0600

    Remove brittle tests
    * The Title is not an output of the component.

--- tests/acceptance/component_test.js
+++ tests/acceptance/component_test.js
@@ -13,11 +13,7 @@ test('component output is rendered', function(){
   expect(3);
 
   visit('/component-test').then(function(){
-    var title = find('h2#title');
     var list = find('.pretty-color');
-
-    equal(title.text(), 'Welcome to Ember.js');
-
     equal(list.length, 3);
     equal(list.first().text(), 'Pretty Color: purple\n');
   });
