387fe1b0ae07886922b488bd880c1929f10003cd
Stefan Penner <stefan.penner@gmail.com>
2014-04-11 08:34:18 -0400

    OCD

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 import Resolver from 'ember/resolver';
 
 var App = Ember.Application.extend({
-  LOG_ACTIVE_GENERATION: true,
-  LOG_MODULE_RESOLVER: true,
-  LOG_TRANSITIONS: true,
+  LOG_ACTIVE_GENERATION:    true,
+  LOG_MODULE_RESOLVER:      true,
+  LOG_TRANSITIONS:          true,
   LOG_TRANSITIONS_INTERNAL: true,
-  LOG_VIEW_LOOKUPS: true,
+  LOG_VIEW_LOOKUPS:         true,
   modulePrefix: 'appkit', // TODO: loaded via config
   Resolver: Resolver
 });
