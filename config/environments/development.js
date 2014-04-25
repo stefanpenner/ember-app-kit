2cb1e220d91ed736f2c801abeded60ea7b0b020f
Cory Forsyth <cory.forsyth@gmail.com>
2014-04-25 18:09:34 -0400

    Use ENV.APP for application log settings. Similar to ember-cli
    See: https://github.com/stefanpenner/ember-cli/blob/master/blueprint/app/index.html#L20

--- config/environments/development.js
+++ config/environments/development.js
@@ -4,3 +4,11 @@
 // endpoint in development than in production.
 //
 // window.ENV.public_key = '123456'
+
+window.ENV.APP = {
+  LOG_ACTIVE_GENERATION:    true,
+  LOG_MODULE_RESOLVER:      true,
+  LOG_TRANSITIONS:          true,
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_VIEW_LOOKUPS:         true
+};
