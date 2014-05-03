ad19f50fc2dd70ad5a9c1c970ffa12aa79620f9d
selvaganesh.b <selvaganesh.b@zohocorp.com>
2014-05-03 17:43:32 +0530

    Add context to exists helper

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -7,8 +7,8 @@ require('ember-qunit').setResolver(resolver);
 window.startApp          = require('appkit/tests/helpers/start-app')['default'];
 window.isolatedContainer = require('ember-qunit/isolated-container')['default'];
 
-function exists(selector) {
-  return !!find(selector).length;
+function exists(selector, context) {
+  return !!find(selector, context).length;
 }
 
 function getAssertionMessage(actual, expected, message) {
